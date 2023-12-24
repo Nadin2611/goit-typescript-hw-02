cbb44ac30a0d46ac749f6cd4f0166438bd32121d
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-12-24 14:17:56 +0200

    6

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,14 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component<T> {
-  constructor(public props: T) {}
-}
+// class Component<T> {
+//   constructor(public props: T) {}
+// }
 
-class Page extends Component {
-  pageInfo() {
-    console.log(this.props.title);
-  }
-}
+// class Page extends Component {
+//   pageInfo() {
+//     console.log(this.props.title);
+//   }
+// }
 
 export {};
